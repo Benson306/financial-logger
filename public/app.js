d74c33b8ea0970b84dd00f939091c69923c2459e
Benson Ndiwa <bnkimtai@gmail.com>
2023-01-30 15:51:27 +0300

    interface

--- public/app.js
+++ public/app.js
@@ -1,4 +1,20 @@
 import { Invoice } from './classes/invoice.js';
+const me = {
+    name: 'Shaun',
+    age: 40,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent ', amount);
+        return amount;
+    }
+};
+let someone;
+const greet = (person) => {
+    console.log(person);
+};
+greet(me);
 const invoiceOne = new Invoice("Benji", "Fees", 50000);
 const invoiceTwo = new Invoice("Luigi", "Fees", 30000);
 let invoices = [];
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,35 @@
 import { Invoice } from './classes/invoice.js';
 
+//interfaces
+interface IsPerson {
+    name: string;
+    age: number;
+    speak(a: string): void;
+    spend(a: number): number;
+}
+
+const me: IsPerson = {
+    name:'Shaun',
+    age: 40,
+    speak(text: string): void{
+        console.log(text)
+    },
+    spend(amount: number): number{
+        console.log('I spent ', amount);
+        return amount;
+    }
+};
+
+let someone: IsPerson ;
+
+const greet = (person: IsPerson) => {
+    console.log(person);
+}
+
+greet(me);
+
+
+
 const invoiceOne = new Invoice("Benji","Fees",50000);
 const invoiceTwo = new Invoice("Luigi","Fees",30000);
 
