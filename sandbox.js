b96b9c6c86e45e8f18e077f10f52538cc30001b4
Benson Ndiwa <bnkimtai@gmail.com>
2023-01-30 10:50:25 +0300

    arrays and objects

--- sandbox.js
+++ sandbox.js
@@ -1,9 +1,26 @@
-var character = "ben";
-var age = 30;
-var isBlackBelt = false;
-character = 'luigi';
-var circ = function (diameter) {
-    return diameter * Math.PI;
-};
+// let character = "ben";
+// let age = 30;
+// let isBlackBelt = false;
+// character = 'luigi';
+// const circ = (diameter: number) =>{
+//     return diameter * Math.PI;
+// }
 //console.log(circ('hello'));
-console.log(circ(4));
+//console.log(circ(4));
+//arrays
+var names = ['luigi', 'mario', 'yoshi'];
+names.push('toad');
+//names.push(20)
+var mixed = ['ben', 4, 'abu', 16];
+//mixed.push(true);
+//objects
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+ninja.age = 20;
+//ninja.age = 'old';
+//Youa cant also add additional properties to an object e.g
+//ninja.skills = ['fighting', 'killing']
+console.log(ninja);
--- sandbox.ts
+++ sandbox.ts
@@ -1,15 +1,42 @@
-let character = "ben";
+// let character = "ben";
 
-let age = 30;
+// let age = 30;
 
-let isBlackBelt = false;
+// let isBlackBelt = false;
 
-character = 'luigi';
+// character = 'luigi';
 
-const circ = (diameter: number) =>{
-    return diameter * Math.PI;
-}
+// const circ = (diameter: number) =>{
+//     return diameter * Math.PI;
+// }
 
 
 //console.log(circ('hello'));
-console.log(circ(4));
+//console.log(circ(4));
+
+    //arrays
+let names = ['luigi','mario','yoshi'];
+
+names.push('toad');
+//names.push(20)
+
+let mixed = ['ben', 4, 'abu', 16];
+
+//mixed.push(true);
+
+    //objects
+let ninja = {
+    name:'mario',
+    belt:'black',
+    age: 30
+}
+
+ninja.age = 20;
+
+//ninja.age = 'old';
+
+//Youa cant also add additional properties to an object e.g
+
+//ninja.skills = ['fighting', 'killing']
+
+console.log(ninja)
